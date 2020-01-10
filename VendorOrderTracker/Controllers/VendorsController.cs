f5b528a603fca9b0801ba9d432796338d17c4161
Nina Potrebich <potrebich@gmail.com>
2020-01-10 14:29:59 -0800

    Add Delete options to vendor

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using VendorOrderTracker.Models;
+using System.Windows;
 
 namespace VendorOrderTracker.Controllers
 {
@@ -21,21 +22,39 @@ namespace VendorOrderTracker.Controllers
     }
 
     [HttpPost("/vendors")]
-    public ActionResult Create(string vendorName, string vendorDescription)
+    public ActionResult Create(string vendorName, string vendorDescription, bool deleteVendors)
     {
-      Vendor newVendor = new Vendor(vendorName, vendorDescription);
-      return RedirectToAction("Index");
+        if (deleteVendors)
+        {
+            Vendor.ClearAll();
+        }
+        else if (vendorName != null)
+        {
+            Vendor newVendor = new Vendor(vendorName, vendorDescription);
+        }
+        
+        return RedirectToAction("Index");
     }
 
     [HttpGet("/vendors/{id}")]
     public ActionResult Show(int id)
     {
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      Vendor selectedVendor = Vendor.Find(id);
-      List<Order> VendorOrders = selectedVendor.Orders;
-      model.Add("vendor", selectedVendor);
-      model.Add("orders", VendorOrders);
-      return View(model);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Vendor selectedVendor = null;
+        List<Order> VendorOrders = null;
+        if (id != -1)
+        {
+            selectedVendor = Vendor.Find(id);
+            if (selectedVendor != null)
+            {
+                VendorOrders = selectedVendor.Orders;
+            }
+            
+        }
+
+        model.Add("vendor", selectedVendor);
+        model.Add("orders", VendorOrders);
+        return View(model);
     }
 
     [HttpGet("/vendors/{id}/edit")]
@@ -46,12 +65,43 @@ namespace VendorOrderTracker.Controllers
     }
 
     [HttpPost("/vendors/{id}")]
-    public ActionResult Update(int id, string updatedVendorName, string updatedVendorDescription)
+    public ActionResult Update(int id, string updatedVendorName, string updatedVendorDescription, bool deleteVendor)
+    {
+        if (!deleteVendor)
+        {
+            Vendor foundVendor = Vendor.Find(id);
+            if (updatedVendorName != null)
+            {
+                foundVendor.Name = updatedVendorName;
+            }
+            if (updatedVendorDescription != null)
+            {
+                foundVendor.Description = updatedVendorDescription;
+            }
+            
+        }
+        else
+        {
+            Vendor.Delete(id);
+            id =-1;
+        }
+
+        
+        return RedirectToAction("Show", id);
+    }
+
+    
+    [HttpGet("/vendors/{id}/delete")]
+    public ActionResult Destroy(int id)
     {
         Vendor foundVendor = Vendor.Find(id);
-        foundVendor.Name = updatedVendorName;
-        foundVendor.Description = updatedVendorDescription;
-        return RedirectToAction("Show", id);
+        return View(foundVendor);
+    }
+
+    [HttpGet("/vendors/delete")]
+    public ActionResult Delete()
+    {
+        return View();
     }
   }
 }
